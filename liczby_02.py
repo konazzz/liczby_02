9f8be49b9788fd57cfbcf05fcda17af5fdbbf306
konazzz <vh33to@gmail.com>
2024-05-19 18:45:36 +0200

    added exceptions

--- liczby_02.py
+++ liczby_02.py
@@ -7,21 +7,33 @@ Think of a number from 1 to 1000
 and i will guess it in less than 10 attempts
 """)
 
+min_1 = 0
+max_1 = 1000
+
 while guessed is not True:
-    min_1 = 0
-    max_1 = 1000
     guess = int((max_1 - min_1) / 2) + min_1
     print(f"My guess is: {guess}")
+    correct_responses = ('too big', 'too small', 'you guessed')
 
-    response = input("pick 'too big', 'too small' or 'you guessed'")
+    response = input("pick 'too big', 'too small' or 'you guessed' ")
+    try:
+        if response not in correct_responses:
+            raise ValueError
+    except ValueError:
+        print("incorrect response")
+        continue
 
     if response == "you guessed":
         print("i won!")
         guessed = True
-    elif response == "too big":
+    elif response == "too big" and max_1 >= guess:
         max_1 = guess
         continue
-    elif response == "too small":
+    elif response == "too small" and min_1 <= guess:
         min_1 = guess
         continue
+    else:
+        print("don't cheat")
+        continue
 
+# i still need to polish the "dont cheat" part
